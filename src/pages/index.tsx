5ec2d6589caac0b702c4e2d62e7ae36168cfa96c
MauriceNino <mauriceprivat98@gmail.com>
2021-09-08 13:38:40 +0200

    add alternatives for SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import Head from 'next/head';
 import React, { useEffect, useState } from 'react';
+import { i18n } from '../../next-i18next.config';
 import { CellProps } from '../types/default-props';
 import HomePage from './home.page';
 
@@ -56,10 +57,19 @@ const App = () => {
       <Head>
         <title>Maurice el-Banna //Portfolio</title>
         <link rel="icon" href="favicon.ico" />
+        <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content={t('meta.description')} />
         <link rel="apple-touch-icon" href="logo192.png" />
         <link rel="manifest" href="manifest.json" />
+        {i18n.locales.map(locale => (
+          <link
+            key={locale}
+            rel="alternate"
+            hrefLang={locale}
+            href={`https://mauz.io/${locale}`}
+          />
+        ))}
       </Head>
 
       <HomePage vCells={state.vCells} hCells={state.hCells} />
